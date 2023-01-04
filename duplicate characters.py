eaa019af7d008366d6dceeaab6d1a4bf4c0a5786
Taran Grover <tarangrover101@gmail.com>
2023-01-04 12:06:50 +0530

    updated leap yera and dup char

--- duplicate characters.py
+++ duplicate characters.py
@@ -1,4 +1,4 @@
-str1 = input("Enter a string: ")
+str1 = input("Enter a string: ").lower()
 dups = []
 for i in str1:
     if str1.count(i) > 1 and i not in dups:
--- leap yera.py
+++ leap yera.py
@@ -1,5 +1,8 @@
+# Condition for a leap year
+# 1. Should be divisible by 4 and not 100 (XXXX)
+# 2. Should be divisible by 400 (XX00)
 year = int(input("Enter year: "))
-if year%4==0:
+if (year%4==0 and year%100 != 0) or (year%400):
     print(year, " is a leap year.")
 else:
     print(year, " is not a leap year.")
