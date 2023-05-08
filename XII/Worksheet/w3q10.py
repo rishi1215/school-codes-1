3a715e906c6379190824778d0cc26a83c6c44b19
AdiGro <tarangrover101@gmail.com>
2023-05-08 04:38:16 +0000

    ques 10 done

--- XII/Worksheet/w3q10.py
+++ XII/Worksheet/w3q10.py
@@ -1,1 +1,34 @@
-# not enough data in question
+from pickle import load
+
+
+def createSampleData() -> None:
+    import random
+    from pickle import dump
+    file = open("doctors.dat", "wb")
+    names = ["Arun", "Bhaskar", "Chandru", "Dinesh", "Eswar", "Fayaz", "Ganesh", "Hari", "Irfan", "Jagan"]
+    for i in range(10):
+        dump([i, "Dr. " + random.choice(names)], file)
+
+
+def details(n) -> None:
+    try:
+        file = open("doctors.dat", "rb")
+    except FileNotFoundError:
+        choice = input("File not found! Create sample data? (y/n): ")
+        if choice == "y":
+            createSampleData()
+            file = open("doctors.dat", "rb")
+        else:
+            return
+    while True:
+        try:
+            doctor = load(file)
+            if doctor[0] == n:
+                print("Doctor ID:", doctor[0])
+                print("Doctor Name:", doctor[1])
+                break
+        except EOFError:
+            print("Doctor not found!")
+            break
+        
+details(int(input("Enter doctor ID: ")))
